0933a53f1df9f81c5c87ce9e6bbc2c77db48a7fa
Tariqul Islam <cavidpark@gmail.com>
2020-12-02 12:54:39 +0600

    adding views and urls

--- english_api/views.py
+++ english_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Word
+from .serializers import WordSerializer
 
 # Create your views here.
+
+class WordList(APIView):
+    """View to list all words in database"""
+    def get(self, request):
+        words = Word.objects.all()
+        data = WordSerializer(words, many=True).data
+        return Response(data)
+
