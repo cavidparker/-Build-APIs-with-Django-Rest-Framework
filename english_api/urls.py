5a5e2b4599e834c684b512a2b0d0120753e29814
Tariqul Islam <cavidpark@gmail.com>
2020-12-02 15:49:06 +0600

    added generic view to post method simple

--- english_api/urls.py
+++ english_api/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path
-# from .views import WordList
-from . import generic_views 
+# This one for a api_views
+# from .api_views import WordList
+
+#This one for generic views
+# from . import generic_views 
+
+from rest_framework import routers
+from . import viewset
+
+router = routers.DefaultRouter()
+router.register('english_api', viewset.WordViewSet)
 
 urlpatterns = [
+    # This one for a api_views
     # path('words/', WordList.as_view(), name = 'word_list')
-    path('', generic_views.ListCreateWord.as_view(), name='word_list'),
-    path('<int:pk>', generic_views.RetrieveUpdateDestroyWord.as_view(), name='word_detail'),
+
+    #This one for generic views
+    # path('', generic_views.ListCreateWord.as_view(), name='word_list'),
+    # path('<int:pk>', generic_views.RetrieveUpdateDestroyWord.as_view(), name='word_detail'),
 ]
+
+urlpatterns += router.urls
