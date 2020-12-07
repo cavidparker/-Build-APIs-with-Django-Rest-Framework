485c10c54f0489683c99eb834057aa8d88f7ca6e
Tariqul Islam <cavidpark@gmail.com>
2020-12-07 18:42:44 +0600

    3rd commit

--- english_api/urls.py
+++ english_api/urls.py
@@ -5,11 +5,19 @@ from django.urls import path
 #This one for generic views
 # from . import generic_views 
 
+# ## This one for viwsets
+# from rest_framework import routers
+# from . import viewset
+# router = routers.DefaultRouter()
+# router.register('english_api', viewset.WordViewSet)
+
+
+## This one for mixins
+from . import mixins
 from rest_framework import routers
-from . import viewset
 
 router = routers.DefaultRouter()
-router.register('english_api', viewset.WordViewSet)
+router.register('english_api', mixins.WordViewSet)
 
 urlpatterns = [
     # This one for a api_views
@@ -19,5 +27,5 @@ urlpatterns = [
     # path('', generic_views.ListCreateWord.as_view(), name='word_list'),
     # path('<int:pk>', generic_views.RetrieveUpdateDestroyWord.as_view(), name='word_detail'),
 ]
-
+## this one for viewsets and mixins
 urlpatterns += router.urls
